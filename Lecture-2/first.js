00a91a3fabf5d3c59c3197d124f96eab519b75d2
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2024-11-29 08:21:44 +0530

    second commit

--- Lecture-2/first.js
+++ Lecture-2/first.js
@@ -1,1 +1,7 @@
 console.log("Alok is the best");
+
+const fs = require("fs");
+fs.writeFile("output.txt", "writing File", (err) => {
+  if (err) throw err;
+  else console.log("File written successfully");
+});
