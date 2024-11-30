4a17d3ec89f52f1b89fe158e6c449c72405c406a
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2024-11-30 17:02:31 +0530

    response and request

--- Lecture-4/user.js
+++ Lecture-4/user.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
@@ -7,9 +8,28 @@ const server = http.createServer((req, res) => {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>Complete Coding</title></head>");
-    res.write("<body><h1>Welcome to Home</h1></body>");
+    res.write("<body><h1>Enter your Details:</h1>");
+    res.write("<form action='/submit-details' method='POST'>");
+    res.write(
+      "<input type='text' name='username' placeholder='Enter your name'>"
+    );
+    res.write("<label for='gender'>Gender:</label>");
+    res.write("<label for='male'>Male</label>");
+    res.write("<input type='radio' id='male' name='gender' value='Male'/>");
+    res.write("<label for='female>Female</label>");
+    res.write("<input type='radio' id='female' name='gender' value='Female'/>");
+    res.write("<button type='submit'>Submit</button>");
+    res.write("</form>");
+    res.write("</body>");
     res.write("</html>");
     return res.end();
+  } else if (
+    req.url.toLowerCase() === "/submit-details" &&
+    req.method === "POST"
+  ) {
+    fs.writeFileSync("user.txt", "Alok Kumar Giri");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
